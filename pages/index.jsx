53b26fd607eb531a5ca8aed0639cc180f034ecb8
sakura miyamoto <sakura.m.208@gmail.com>
2021-10-08 18:58:35 +0900

    仮デプロイ

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,65 +4,91 @@ import Logo from "../public/meshiran_logo-03.png";
 import { useState } from "react";
 import Layout from "../components/layoutParts/Layout";
 
-const index = () => {
+const Index = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  console.log(email);
+  console.log(password);
+  // localStorage.setItem("email", email);
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      // await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/login`, {
+      await fetch("https://nobucom.sakura.ne.jp/meshiran/public/api/login", {
+        method: "POST",
+        body: {
+          email: email,
+          password: password,
+        },
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          if (res.status === 400) {
+            throw "認証が失敗しました";
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          localStorage.setItem("access_token", data);
+        });
+      // router.push("/main");
+    } catch (err) {
+      alert(err);
+    }
+  };
 
   return (
     <Layout title="飯ラン">
-      <div className="p-5">
-        <div className="text-center">
-          <Image src={Logo} alt="logo" width="200" height="200" />
-          <p className="text-center mb-5">メールアドレスでログイン</p>
+      <form onSubmit={login}>
+        <div className="p-5">
+          <div className="text-center">
+            <Image src={Logo} alt="logo" width="200" height="200" />
+            <p className="text-center mb-5">メールアドレスでログイン</p>
+          </div>
+          <p className="mb-2">メールアドレス</p>
+          <input
+            className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-red-500 focus:border-red-500 focus:z-10 sm:text-sm mb-6"
+            placeholder="例）xxx@xxxco.jp"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <p className="mb-2">パスワード</p>
+          <input
+            className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-red-500 focus:border-red-500 focus:z-10 sm:text-sm"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <p className="text-gray-400">5文字以上の英数字</p>
         </div>
-        <p className="mb-2">メールアドレス</p>
-        <input
-          name="email"
-          type="email"
-          autoComplete="email"
-          required
-          className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-red-500 focus:border-red-500 focus:z-10 sm:text-sm mb-6"
-          placeholder="例）xxx@xxxco.jp"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <p className="mb-2">パスワード</p>
-        <input
-          name="password"
-          type="password"
-          autoComplete="password"
-          required
-          className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-red-500 focus:border-red-500 focus:z-10 sm:text-sm"
-          placeholder=""
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <p className="text-gray-400">5文字以上の英数字</p>
-      </div>
-      <div className="flex flex-col items-center justify-between mt-2 px-3">
-        <button
-          type="submit"
-          className="text-lg hover:bg-red-700 text-white font-bold py-2 px-16 rounded"
-          style={{ background: "#f00a00" }}
-        >
-          ログイン
-        </button>
-        <p className="text-center mt-6 mb-2">はじめてご利用の方</p>
-        <Link href="/register" passHref>
+        <div className="flex flex-col items-center justify-between mt-2 px-3">
           <button
-            className="text-lg hover:bg-yellow-700 text-white font-bold py-2 px-9 rounded"
-            style={{ background: "#ff8c00" }}
+            type="submit"
+            className="text-lg hover:bg-red-700 text-white font-bold py-2 px-16 rounded"
+            style={{ background: "#f00a00" }}
           >
-            会員登録に進む
+            ログイン
           </button>
-        </Link>
-      </div>
+          <p className="text-center mt-6 mb-2">はじめてご利用の方</p>
+          <Link href="/register" passHref>
+            <button
+              className="text-lg hover:bg-yellow-700 text-white font-bold py-2 px-9 rounded"
+              style={{ background: "#ff8c00" }}
+            >
+              会員登録に進む
+            </button>
+          </Link>
+        </div>
+      </form>
     </Layout>
   );
 };
 
-export default index;
+export default Index;
